129c78c8e75410abe0d728d8fabfb6c89fdef359
Nathan King <nkvherus@gmail.com>
2016-01-07 14:16:15 +0000

    First pass implementation of base Lookup model

---
+++ lib/Mxtb/Model/Lookup/Lookup.php
@@ -0,0 +1,67 @@
+<?php declare(strict_types = 1);
+
+/**
+ * This file is part of the core PHP package for mxtoolbox-php-api.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @package mxtoolbox-php-api
+ * @author Nathan King <nkvherus@gmail.com>
+ * @version dev
+ */
+
+namespace Mxtb\Model\Lookup;
+
+class Lookup
+{
+    /**
+     * @var string
+     */
+    private $uid;
+
+    /**
+     * @var string
+     */
+    private $command;
+
+    /**
+     * @var bool
+     */
+    private $isTransitioned;
+
+    /**
+     * @var string
+     */
+    private $commandArgument;
+
+    /**
+     * @var string
+     */
+    private $timeRecorded;
+
+    /**
+     * @var string|null
+     */
+    private $reportingNameServer;
+
+    /**
+     * @var string
+     */
+    private $timeToComplete;
+
+    /**
+     * @var string
+     */
+    private $relatedIP;
+
+    /**
+     * @var bool
+     */
+    private $isEndpoint;
+
+    /**
+     * @var bool
+     */
+    private $hasSubscriptions;
+}
