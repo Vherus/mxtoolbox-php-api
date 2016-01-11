1a172c78ba272b4155b607f1133e86154d82228d
Darien <daz.livermore@hotmail.com>
2016-01-11 09:28:20 +0000

    Added initial PHPdoc.

--- test/Mxtb/Tests/Model/Lookup/Mx/ErrorsResponseTest.php
+++ test/Mxtb/Tests/Model/Lookup/Mx/ErrorsResponseTest.php
@@ -1,5 +1,16 @@
 <?php
 
+/**
+ * This file is part of the core PHP package for mxtoolbox-php-api.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @package mxtoolbox-php-api
+ * @author Darien Livermore <daz.livermore@hotmail.com>
+ * @version dev
+ */
+
 namespace Mxtb\Tests\Model\Lookup\Mx;
 
 use Mxtb\Model\Lookup\Mx\ErrorsResponse;
