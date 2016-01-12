f9e9497159824a699b1cd402959a946c9ae33005
Darien <daz.livermore@hotmail.com>
2016-01-12 09:47:25 +0000

    Added information response for lookup type Txt.

---
+++ lib/Mxtb/Model/Lookup/Txt/InformationResponse.php
@@ -0,0 +1,85 @@
+<?php declare(strict_types = 1);
+
+/**
+ * This file is part of the core PHP package for mxtoolbox-php-api.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @package mxtoolbox-php-api
+ * @author Darien Livermore <daz.livermore@hotmail.com>
+ * @version dev
+ */
+ 
+namespace Mxtb\Model\Lookup\Txt;
+
+use Mxtb\Model\Lookup\AbstractInformationResponse;
+
+class InformationResponse extends AbstractInformationResponse
+{
+    /**
+     * @var string|null
+     */
+    private $domainName;
+	
+    /**
+     * @var string|null
+     */
+    private $ttl;
+	
+    /**
+     * @var string|null
+     */
+    private $record;
+	
+    /**
+     * @return string|null
+     */
+    public function getDomainName() : string
+    {
+        return $this->domainName;
+    }
+
+    /**
+     * @param string|null $domainName
+     */
+    public function setDomainName(string $domainName) : InformationResponse
+    {
+        $this->domainName = $domainName;
+        return $this;
+    }
+	
+    /**
+     * @return string|null
+     */
+    public function getTTL() : string
+    {
+        return $this->ttl;
+    }
+
+    /**
+     * @param string|null $ttl
+     */
+    public function setTTL(string $ttl) : InformationResponse
+    {
+        $this->ttl = $ttl;
+        return $this;
+    }
+	
+    /**
+     * @return string|null
+     */
+    public function getRecord() : string
+    {
+        return $this->record;
+    }
+
+    /**
+     * @param string|null $record
+     */
+    public function setRecord(string $record) : InformationResponse
+    {
+        $this->record = $record;
+        return $this;
+    }
+}
