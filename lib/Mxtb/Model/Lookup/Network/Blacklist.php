f9f3ebcbf914617ff99dcc216abae6425a927ccf
Darien <daz.livermore@hotmail.com>
2016-01-13 12:16:43 +0000

    Merge.

--- lib/Mxtb/Model/Lookup/Network/Blacklist.php
+++ lib/Mxtb/Model/Lookup/Network/Blacklist.php
@@ -10,26 +10,26 @@
  * @author Nathan King (nkvherus@gmail.com)
  * @copyright 2016 Nathan King (nkvherus@gmail.com)
  */
-
 namespace Mxtb\Model\Lookup\Network;
-
 use JMS\Serializer\Annotation\Type;
 use JMS\Serializer\Annotation\SerializedName;
 use JMS\Serializer\Annotation\Accessor;
+use Mxtb\Model\Collection\Failed;
+use Mxtb\Model\Collection\Information;
 use Mxtb\Model\Collection\Passed;
 use Mxtb\Model\Lookup\Network\Blacklist\FailedResponse;
 use Mxtb\Model\Lookup\Network\Blacklist\PassedResponse;
 use Mxtb\Model\Lookup\Network\Blacklist\InformationResponse;
-
 class Blacklist extends AbstractNetworkLookup
 {
     /**
+     * @var Failed
+     *
      * @Type("array<Mxtb\Model\Lookup\Network\Blacklist\FailedResponse>")
      * @SerializedName("Failed")
      * @Accessor(getter="getFailed",setter="setFailed")
      */
     private $failed;
-
     /**
      * @var Passed
      *
@@ -38,32 +38,30 @@ class Blacklist extends AbstractNetworkLookup
      * @Accessor(getter="getPassed",setter="setPassed")
      */
     private $passed;
-
     /**
+     * @var Information
+     *
      * @Type("array<Mxtb\Model\Lookup\Network\Blacklist\InformationResponse>")
-     * @SerializedName("Information")
+     * @SerializedName("Passed")
      * @Accessor(getter="getInformation",setter="setInformation")
      */
     private $information;
-
     /**
-     * @return FailedResponse[]|null
+     * @return Failed|null
      */
     public function getFailed()
     {
         return $this->failed;
     }
-
     /**
      * @param FailedResponse[]|null $failed
      * @return Blacklist
      */
     public function setFailed(array $failed = null) : Blacklist
     {
-        $this->failed = $failed;
+        $this->failed = new Failed($failed);
         return $this;
     }
-
     /**
      * @return Passed|null
      */
@@ -71,7 +69,6 @@ class Blacklist extends AbstractNetworkLookup
     {
         return $this->passed;
     }
-
     /**
      * @param PassedResponse[]|null $passed
      * @return Blacklist
@@ -79,25 +76,22 @@ class Blacklist extends AbstractNetworkLookup
     public function setPassed(array $passed = null) : Blacklist
     {
         $this->passed = new Passed($passed);
-//        $this->passed = $passed;
         return $this;
     }
-
     /**
-     * @return InformationResponse[]|null
+     * @return Information|null
      */
     public function getInformation()
     {
         return $this->information;
     }
-
     /**
      * @param InformationResponse[]|null $information
      * @return Blacklist
      */
     public function setInformation(array $information = null) : Blacklist
     {
-        $this->information = $information;
+        $this->information = new Information($information);
         return $this;
     }
 }
